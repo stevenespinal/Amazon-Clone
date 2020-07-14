a6f37e5d1b7bf70978221f1ab6bffff15d1acfba
Steven Espinal <stevenjesusespinal@gmail.com>
2020-07-14 06:32:25 -0400

    Initial files, completed navbar

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital" alt=""/>
+    </div>
+  )
+}
+
+export default Home;
